72a76f5350f7227b40506b42b17a74a278e3ab42
Chris LoPresto <chris.lopresto@betterment.com>
2016-08-25 09:31:09 -0400

    Add resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'bateman', to: 'bateman#index'
+  get 'resume', to: 'resume#index'
   get ':ember_island', to: 'ember_islands#index', constraints: { ember_island: /living-style-guide-driven-development|ember-modal-dialog-intro/ }
   namespace :presentations do
     get 'living_style_guide_driven_development', to: 'living_style_guide_driven_development#index'
