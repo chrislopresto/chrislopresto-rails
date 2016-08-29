59983b2249b664eb8fcd7aa698e07d18678b13bd
Chris LoPresto <chris.lopresto@betterment.com>
2016-08-29 01:09:31 -0400

    Add hello world react component

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,3 +1,10 @@
 class SiteController < ApplicationController
   layout 'site'
+
+  def index
+    @hello_world_props = {
+      name: 'Chris LoPresto',
+      traits: ['software engineer', 'musician']
+    }
+  end
 end
